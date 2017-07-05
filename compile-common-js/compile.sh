9054e24129ddc215ca9b9736ffd13b75a41c0790
Alex Pattison <alexlpattison@gmail.com>
2017-07-05 15:20:31 -0700

    Add comments for question

--- compile-common-js/compile.sh
+++ compile-common-js/compile.sh
@@ -2,16 +2,24 @@
 
 set -eu
 
+# set proj equal to current directory?
+# when I try evaluating the assignment in the console I end up with "dirname path"
 proj=$(dirname "$0")/..
+# cd into proj
 cd "$proj"
 
+# remove existing cjs before repopulating
 rm -rf cjs
 
+# print to console
 echo "Compiling to CommonJS..."
+
+# loop through (not sure quite what)
 for f in $(find src -name "*.js")
 do
     in=$f
     out=${f/src/cjs}
+    # make each of the CommonJS directories
     mkdir -p $(dirname $out)
     echo "  $in > $out"
     node_modules/.bin/babel $in > $out
